d8e5aa5138eb4df1ff345b4f1c72a05f9d59b432
dinar1122 <gert.olh@mail.ru>
2025-04-24 10:39:49 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,41 @@
-import react from "@vitejs/plugin-react"
-import * as path from "node:path"
-import { defineConfig } from "vitest/config"
-import packageJson from "./package.json" with { type: "json" }
+import react from "@vitejs/plugin-react";
+import * as path from "node:path";
+import { defineConfig } from "vitest/config";
+import packageJson from "./package.json" with { type: "json" };
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/atlas/' : '/',
+  base: '/atlas/todo/',
   server: {
     open: true,
   },
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   test: {
     root: import.meta.dirname,
     name: packageJson.name,
     environment: "jsdom",
-
     typecheck: {
       enabled: true,
       tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
     },
-
     globals: true,
     watch: false,
     setupFiles: ["./src/setupTests.ts"],
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
